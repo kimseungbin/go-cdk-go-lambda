d7cefe5bc587874204abf85690394c879ef67fc1
SeungBin Kim <luxorienskim@gmail.com>
2023-06-12 15:09:58 +0900

    feat: lambda to copy file from original bucket to destination bucket

--- lambda/go.mod
+++ lambda/go.mod
@@ -2,4 +2,9 @@ module main
 
 go 1.20
 
-require github.com/aws/aws-lambda-go v1.41.0
+require (
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/aws/aws-sdk-go v1.44.280
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- lambda/main.go
+++ lambda/main.go
@@ -5,16 +5,39 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"os"
+	"strings"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (response events.APIGatewayProxyResponse, err error) {
-	fmt.Printf("request: %v", request.Body)
+func handler(ctx context.Context, s3Event events.S3Event) (err error) {
+	s3EventRecord := s3Event.Records[0].S3
+	sourceBucket := s3EventRecord.Bucket.Name
+	sourceKey := s3EventRecord.Object.Key
 
-	response = events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Headers:    map[string]string{"Content-Type": "text/plain"},
-		Body:       "Hello, World!",
+	destinationBucket := os.Getenv("RESIZED_BUCKET_NAME")
+
+	fmt.Printf("Copy %s", sourceKey)
+
+	// Create a new AWS session
+	sess := session.Must(session.NewSession())
+
+	// Create an S3 client
+	s3client := s3.New(sess)
+
+	// Copy the object to the destination bucket
+	_, err = s3client.CopyObjectWithContext(ctx, &s3.CopyObjectInput{
+		CopySource: aws.String(strings.Join([]string{sourceBucket, sourceKey}, "/")),
+		Bucket:     aws.String(destinationBucket),
+		Key:        aws.String(sourceKey),
+	})
+	if err != nil {
+		return
 	}
+
+	fmt.Printf("File copied from %s/%s to %s/%s", sourceBucket, sourceKey, destinationBucket, sourceKey)
 
 	return
 }
